53118cd05c2a90f4ce4d627d146f726c8c842431
shivamgill9322 <shivamgill9322@gmail.com>
2025-04-23 11:55:34 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,23 +1,4 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-# Load the Excel file into a DataFra
-data_path = r"C:\Users\ASUS\OneDrive\Desktop\DataSet Employment.xlsx"
-df = pd.read_excel(data_path)
-# Set up a plot style
-sns.set(style="whitegrid")
-# 1. Histogram  
-plt.figure(figsize=(6, 4))
-sns.histplot(df["Employment Rate"], kde=True, color="skyblue", bins=10)
-plt.title("Histogram of Employment Rate")
-plt.xlabel("Employment Rate")
-plt.ylabel("Frequency")
-plt.show()
-# 2. Bar Plot (Average Employment Rate by Region)
-plt.figure(figsize=(6, 4))
-sns.barplot(x="Region", y="Employment Rate", data=df, palette="Set2")
-plt.title("Average Employment Rate by Region")
 plt.xlabel("Region")
 plt.ylabel("Employment Rate")
 plt.xticks(rotation=45)  # In case region labels are long
