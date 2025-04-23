6b959b556fd95dd4b79f5b961aadf40b10092bda
shivamgill9322 <shivamgill9322@gmail.com>
2025-04-23 11:47:20 +0530

    Update python.py

--- python.py
+++ python.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load the Excel file into a DataFrame
+
 data_path = r"C:\Users\ASUS\OneDrive\Desktop\DataSet Employment.xlsx"
 df = pd.read_excel(data_path)
 # Set up a plot style
